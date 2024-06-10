1ca55e9f44b544529dacc214cfd5f34d8b48d91e
Adrian Maulani <maulani.ad@gmail.com>
2024-06-11 00:58:56 +0700

    feat(decryption): new decryption feature

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
+from time import time
+
 from django.contrib import messages
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
 
 from database.models import File, Transaction
-from core.aes128 import encrypt
 from core.blowfish import Blowfish
 from core.checksum import compute_checksum
 from helpers.dates import dt_now
@@ -14,7 +15,9 @@ from helpers.functions import (write_bytes_to_file,
                                generate_random_chars,
                                generate_search_code,
                                format_size,
-                               paginate)
+                               paginate,
+                               encrypt_file,
+                               decrypt_file)
 from helpers.types import ARCHIVES
 
 # Create your views here.
@@ -36,24 +39,33 @@ class EncryptionView(View):
             messages.error(request, message="File dan kunci tidak boleh kosong ...")
             return render(request, template_name="encryption.html")
 
+        aes_key: bytes = generate_random_chars(type=bytes, length=16)  # type: ignore
+        aes_iv: bytes = generate_random_chars(type=bytes, length=16)   # type: ignore
+
+        start_time = time()
+
         new_file = File.objects.create(
             file=file,
             filename=file.name,
             extension=file.name.split(".")[-1],
             size=file.size,
+            aes_key=aes_key,
             secret_key=make_password(key)
         )
 
         file_checksum = compute_checksum(new_file.file.path)
 
-        encrypted_file = encrypt(new_file.file.path, key)
+        encrypted_file = encrypt_file(new_file.file.file, aes_key, aes_iv)
         write_bytes_to_file(encrypted_file, new_file.file.path)
 
+        end_time = time()
+
         blowfish_engine = Blowfish(key)
+        blowfish_validation_key = generate_random_chars(type=str)
 
         new_transaction = Transaction.objects.create(
-            key=blowfish_engine.encrypt(generate_random_chars()),
-            vector=file_checksum,
+            key=blowfish_engine.encrypt(blowfish_validation_key),
+            vector=aes_iv,
             name=generate_search_code(new_file.extension),
             status=TransactionStatus.ENCRYPTED,
             user=request.user,
@@ -61,7 +73,9 @@ class EncryptionView(View):
         )
 
         template_context = {
+            'elapsed_time': round(end_time - start_time, 2),
             'checksum': file_checksum,
+            'validation_key': blowfish_validation_key,
             'oid': new_file.oid,
             'size': format_size(new_file.size),
             'name': new_transaction.name,
@@ -95,26 +109,38 @@ class DecryptionView(View):
     def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         operation = kwargs['operation']
         object_id = request.POST.get('oid', None)
+        secret_key = request.POST.get('key', None)
 
         if not object_id:
             messages.error(request, message="Transaksi atau File tidak ada ...")
             return redirect(to="app:decryption")
 
         if operation == "decrypt":
-            return self._decrypt(request, object_id)
+            return self._decrypt(request, object_id, secret_key)
 
         return self._delete(request, object_id)
 
-    def _decrypt(self, request: HttpRequest, object_id: str) -> HttpResponse:
+    def _decrypt(self, request: HttpRequest, object_id: str, secret_key: str | None) -> HttpResponse:
         try:
             transaction = Transaction.objects.get(oid=object_id)
         except Transaction.DoesNotExist:
             messages.error(request, message="Transaksi atau File tidak ada ...")
             return redirect(to="app:decryption")
 
-        # transaction.status = TransactionStatus.DECRYPTED.value
-        # transaction.save()
+        if not check_password(secret_key, transaction.file.secret_key):
+            messages.error(request, message="Kunci tidak sesuai ...")
+            return redirect(to="app:decryption")
 
+        blowfish_engine = Blowfish(secret_key)
+        decrypted_key = blowfish_engine.decrypt(list(map(ord, transaction.key))).decode("utf-8")
+
+        decrypted_file = decrypt_file(transaction.file.file, transaction.file.aes_key, transaction.vector)
+        write_bytes_to_file(decrypted_file, transaction.file.file.path)
+
+        transaction.status = TransactionStatus.DECRYPTED.value
+        transaction.save()
+
+        messages.success(request, message=f"Dekripsi Berhasil! ... Blowfish Validation Key: {decrypted_key}")
         return redirect(to="app:decryption")
 
     def _delete(self, request: HttpRequest, object_id: str) -> HttpResponse:
