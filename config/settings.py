d8125437a681fbf61e9a4d1d7926a7435e9a697e
Adrian Maulani <maulani.ad@gmail.com>
2024-06-11 01:06:57 +0700

    chore(admin): completely removed django administration page

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.spl
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 from config.errors import ErrorPageView
@@ -26,7 +25,6 @@ handler404 = "config.errors.not_found"
 handler500 = "config.errors.internal_server_error"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("errors/", ErrorPageView.as_view(), name="errors"),
     path("errors/<int:code>/", ErrorPageView.as_view(), name="errors"),
 
