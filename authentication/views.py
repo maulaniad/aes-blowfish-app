4c1bf4395ae8aac9e7e8cb2a1eaff8d268823cdc
Adrian Maulani <maulani.ad@gmail.com>
2024-06-11 01:42:29 +0700

    refactor(auth): tidying up code

--- authentication/views.py
+++ authentication/views.py
@@ -59,50 +59,50 @@ class ResetPasswordView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
         post_type = request.POST.get('type', None)
 
-        if post_type and post_type.lower() == "validation":
-            form = ResetPasswordForm(request.POST)
+        form = ResetPasswordForm(request.POST)
+        if form.is_valid():
+            if post_type and post_type.lower() == "validation":
+                return self._validation(request, form)
 
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                email = form.cleaned_data.get('email')
-                validation_filter = Q(username=username) | Q(email=email)
-                user_data = User.objects.get(validation_filter, date_deleted__gt=dt_now())
-
-                if user_data.password_changed:
-                    allowed_date = user_data.password_changed + timedelta(days=7)
-                    delta = allowed_date - dt_now()
-
-                    if delta.days > 0:
-                        context = {'delta': delta.days - 7}
-                        return render(request, template_name="reset_password.html", context=context)
-
-                context = {
-                    'username': form.cleaned_data.get('username'),
-                    'email': form.cleaned_data.get('email'),
-                    'next': True
-                }
-
-                return render(request, template_name="reset_password.html", context=context)
-
-        elif post_type and post_type.lower() == "change_password":
-            form = ResetPasswordForm(request.POST)
-
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                email = form.cleaned_data.get('email')
-                new_password = form.cleaned_data.get('password')
-
-                validation_filter = Q(username=username) | Q(email=email)
-                user_data = User.objects.get(validation_filter, date_deleted__gt=dt_now())
-                user_data.password = make_password(new_password)
-                user_data.password_changed = dt_now()
-                user_data.save()
-
-                messages.success(request, message="Setel Ulang Kata Sandi Berhasil!")
-                return redirect(to="authentication:login")
+            return self._change_password(request, form)
 
         messages.error(request, message="Setel Ulang Kata Sandi Gagal ...")
         return redirect(to="authentication:reset_password")
+
+    def _validation(self, request: HttpRequest, form: ResetPasswordForm) -> HttpResponse:
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        validation_filter = Q(username=username) | Q(email=email)
+        user_data = User.objects.get(validation_filter, date_deleted__gt=dt_now())
+
+        if user_data.password_changed:
+            allowed_date = user_data.password_changed + timedelta(days=7)
+            delta = allowed_date - dt_now()
+
+            if delta.days > 0:
+                context = {'delta': delta.days - 7}
+                return render(request, template_name="reset_password.html", context=context)
+
+        context = {
+            'username': form.cleaned_data.get('username'),
+            'email': form.cleaned_data.get('email'),
+            'next': True
+        }
+        return render(request, template_name="reset_password.html", context=context)
+
+    def _change_password(self, request: HttpRequest, form: ResetPasswordForm) -> HttpResponse:
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        new_password = form.cleaned_data.get('password')
+
+        validation_filter = Q(username=username) | Q(email=email)
+        user_data = User.objects.get(validation_filter, date_deleted__gt=dt_now())
+        user_data.password = make_password(new_password)
+        user_data.password_changed = dt_now()
+        user_data.save()
+
+        messages.success(request, message="Setel Ulang Kata Sandi Berhasil!")
+        return redirect(to="authentication:login")
 
 
 class LogoutView(View):
